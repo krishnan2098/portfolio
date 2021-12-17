65efab21a1deed0ef33d2725d259b0127709988b
krishnan2098 <krishnan2098@gmail.com>
2021-12-18 01:46:51 +0530

    Changed Link to NavLink to maintain active state for a particular route

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './footer.css';
 import * as AiIcons from 'react-icons/ai';
 
@@ -14,9 +14,9 @@ const Footer = () => {
                     <div className="f-col"><span>Designed and Developed by Shankar</span></div>
                 </div>
                 <div className="f-row list map">
-                    <div className="f-col"><Link className='link' to='/'>Hello</Link></div>
-                    <div className="f-col"><Link className='link' to='/projects'>Projects</Link></div>
-                    <div className="f-col"><Link className='link' to='/blog'>Blog </Link></div>
+                    <div className="f-col"><NavLink className='link' activeClassName='link active' to='/'>Hello</NavLink></div>
+                    <div className="f-col"><NavLink className='link' activeClassName='link active' to='/projects'>Projects</NavLink></div>
+                    <div className="f-col"><NavLink className='link' activeClassName='link active' to='/blog'>Blog </NavLink></div>
                 </div>
                 <div className="f-row list socials">
                     <div onMouseOver={() => setLnActive(true)} onMouseLeave={() => setLnActive(false)} className='social-icon f-col'><a className={lnactive ? 'active' : ''} href='https://www.linkedin.com/in/sankararaman-k/'><AiIcons.AiFillLinkedin size={25} /></a></div>
