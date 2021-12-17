451bdb35338facf559de290774f3642db5c8f6f3
krishnan2098 <krishnan2098@gmail.com>
2021-12-17 15:22:44 +0530

    Implemented Navbar, Intro, Projects, Blog and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/navbar/navbar'
+import Intro from './components/intro/intro'
+import Projects from './components/projects/projects'
+import Blog from './components/blog/blog'
+import Footer from './components/footer/footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/'></Route>
+        </Routes>
+      </Router>
+      <div className='app-wrapper'>
+        {/* <div className='app-row' style={{ backgroundColor: "black"}}>
+          <div className='app-col'>
+            <Intro/>
+          </div>
+        </div> */}
+        {/* <div className='app-row'>
+          <div className='app-col'>
+            <Navbar/>
+          </div>
+        </div> */}
+        <div className='app-row'>
+          <div className='app-col'>
+            <Projects/>
+            <Footer/>
+          </div>
+        </div>
+        <div className='app-row'>
+          <div className='app-col a-blog'>
+            <Blog/>
+            <Footer/>
+          </div>
+        </div>
+      </div>
+      {/* <Footer/> */}
     </div>
-  );
+    );
 }
 
 export default App;
