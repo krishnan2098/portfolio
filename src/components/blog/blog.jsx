dc2044440cddb058ae6c9e9691d3add51fc0a8f1
krishnan2098 <krishnan2098@gmail.com>
2021-12-18 01:43:45 +0530

    Updated Blog component and fixed spacing issues

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -4,45 +4,43 @@ import './blog.css'
 const Blog = () => {
     return (
         <div id="blog" className="blog">
-            {/* <h1>Blog</h1> */}
-            <div className="b-wrapper">
-                <div className="b-row">
-                    <div className="b-col-lg">
-                        <div className="popular-post">
-                            <div className="post-row">
-                                <div className="post-img">
-                                    <img src="https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg"/>
-                                </div>
-                            </div>                        
-                            <div className="post-row meta">
-                                <div className="post-date"><div><span>Feb 11</span></div></div>
-                                <div className="post-author"><span>Al Pachino</span></div>
+            <div className="b-section">
+                <div className="b-wrapper">
+                    <div className="b-left">
+                        <div className="b-row-lg">
+                            <div className="post-img-lg">
+                                <img className="thumbnail" src="https://www.bing.com/th?id=OHR.USSBowfin_EN-US9736688640_1920x1080.jpg&rf=LaDigue_1920x1080.jpg" alt="post-img" />
                             </div>
-                            <div className="post-row">
-                                <div className="post-heading"><h2>This is sample heading for a post</h2></div>
+                            <div className="post-meta">
+                                <span className="meta-col post-date">11 Dec</span>
+                                <span className="meta-col post-author">Al Pacino</span>
                             </div>
-                            <div className="post-row">
-                                <div className="post-summary"><p>This is a sample summary for the post. Summary is usually used to summarise the entire post. In the context of design they are trimmed starters of the post</p></div>
-                            </div>                        
+                            <div className="post-header"><h1>Popular Post Heading </h1></div>
+                            <div className="post-subheading"><p>Enter the world of subheading of Popular Post</p></div>
                         </div>
                     </div>
-                    <div className="b-col">
-                        <div className="suggested-post">
-                            <div className="post-row">
-                                <div className="post-img">                                    
-                                    <img src="https://www.bing.com/th?id=OHR.USSBowfin_EN-US9736688640_1920x1080.jpg&rf=LaDigue_1920x1080.jpg"/>
-                                </div>
-                            </div>                        
-                            <div className="post-row meta">
-                                <div className="post-date"><span>Feb 11</span></div>
-                                <div className="post-author"><span>Al Pachino</span></div>
+                    <div className="b-right">
+                        <div className="b-row">
+                            <div className="post-img">
+                                <img className="thumbnail" src="https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg" alt="post-img" />
                             </div>
-                            <div className="post-row">
-                                <div className="post-heading"><h2>This is sample heading for a post</h2></div>
+                            <div className="post-meta">
+                                <span className="meta-col popular post-date">11 Dec</span>
+                                <span className="meta-col popular post-author">Al Pacino</span>
                             </div>
-                            <div className="post-row">
-                                <div className="post-summary"><p>This is a sample summary for the post. Summary is usually used to summarise the entire post. In the context of design they are trimmed starters of the post</p></div>
-                            </div>                        
+                            <div className="post-header"><h4>Popular Post Heading </h4></div>
+                            <div className="popular post-subheading"><p>Enter the world of subheading of Popular Post</p></div>
+                        </div>
+                        <div className="b-row">
+                            <div className="post-img">
+                                <img className="thumbnail" src="https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg" alt="post-img" />
+                            </div>
+                            <div className="post-meta">
+                                <span className="meta-col popular post-date">11 Dec</span>
+                                <span className="meta-col popular post-author">Al Pacino</span>
+                            </div>
+                            <div className="post-header"><h4>Popular Post Heading </h4></div>
+                            <div className="popular post-subheading"><p>Enter the world of subheading of Popular Post</p></div>
                         </div>
                     </div>
                 </div>
