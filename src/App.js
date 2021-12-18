bf822e8de8265a07ec320849be8d5e82969a90d6
krishnan2098 <krishnan2098@gmail.com>
2021-12-19 04:45:19 +0530

    close #1 Implemented Dark and Light theme switching

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import Navbar from './components/navbar/navbar'
-import Intro from './components/intro/intro'
-import Projects from './components/projects/projects'
-import Blog from './components/blog/blog'
-import Footer from './components/footer/footer'
-import Contact from './components/contact/contact'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import Navbar from './components/navbar/navbar';
+import Intro from './components/intro/intro';
+import Projects from './components/projects/projects';
+import Blog from './components/blog/blog';
+import Footer from './components/footer/footer';
+import Contact from './components/contact/contact';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { ThemeProvider } from 'styled-components';
+import './App.css';
+
+const LightTheme = {
+  pageBackground: "#fff",
+  textColor: "#202020",
+  titleColor: "#FC6D6D",
+  highlightColor: "#FC6D6D",
+  navmenuBackground: "#fff",
+  navmenuHighlightColor: "#efefef",
+  blogDividerColor: "#0c0c0c",
+  scrollBorderColor: "#fff",
+  scrollThumbColor: "#505050",
+  galleryImgNumColor: "#efefef",
+  galleryBorderColor: "#efefef",
+  iconHover: "#0c0c0c",
+};
+
+const DarkTheme = {
+  pageBackground: "#000",
+  textColor: "#bfc4cc",
+  titleColor: "#FC6D6D",
+  highlightColor: "#FC6D6D",
+  navmenuBackground: "#101010",
+  navmenuHighlightColor: "#0c0c0c",
+  blogDividerColor: "#bfc4cc",
+  scrollBorderColor: "#000",
+  scrollThumbColor: "#bfc4cc",
+  galleryImgNumColor: "#303030",
+  galleryBorderColor: "#202020",
+  iconHover: "#efefef",
+};
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme
+};
 
 const App = () => {
+  const updateTheme = (theme) => {
+    document.documentElement.style.setProperty('--pageBackground', theme.pageBackground);
+    document.documentElement.style.setProperty('--textColor', theme.textColor);
+    document.documentElement.style.setProperty('--titleColor', theme.titleColor);
+    document.documentElement.style.setProperty('--highlightColor', theme.highlightColor);
+    document.documentElement.style.setProperty('--navmenuBackground', theme.navmenuBackground);
+    document.documentElement.style.setProperty('--navmenuHighlightColor', theme.navmenuHighlightColor);
+    document.documentElement.style.setProperty('--blogDividerColor', theme.blogDividerColor);
+    document.documentElement.style.setProperty('--scrollBorderColor', theme.scrollBorderColor);
+    document.documentElement.style.setProperty('--scrollThumbColor', theme.scrollThumbColor);
+    document.documentElement.style.setProperty('--galleryImgNumColor', theme.galleryImgNumColor);
+    document.documentElement.style.setProperty('--galleryBorderColor', theme.galleryBorderColor);
+    document.documentElement.style.setProperty('--iconHover', theme.iconHover);
+  }
+
+  const [theme, setTheme] = useState("dark");
   return (
-    <div className='App'>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Intro />} />
-          <Route path='/blog' element={<Blog />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-        <Footer/>
-      </Router>
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <div className='app'>
+        <div className='app-wrapper'>
+          <Router>
+            <Navbar theme={theme} themes={themes} updateTheme={updateTheme} setTheme={setTheme} />
+            <Routes>
+              <Route path='/' element={<Intro />} />
+              <Route path='/blog' element={<Blog />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </div>
+      </div>
+    </ThemeProvider>
     );
 }
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,36 +2,48 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-import * as MdIcons from 'react-icons/md'
+import * as MdIcons from 'react-icons/md';
 import { NavbarMenuData } from './navbar-menu-data';
-import './navbar.css'
+import './navbar.css';
 import Logo from '../../logo-vector.png';
 
-const Navbar = () => {
-    const [navmenu, setNavmenu] = useState(false)
+const Navbar = (props) => {
+    const [navmenu, setNavmenu] = useState(false);
+    const showNavmenu = () => setNavmenu(!navmenu);
 
-    const showNavmenu = () => setNavmenu(!navmenu)
-    
+    const changeTheme = () => {
+        if (props.theme === 'light') {
+            props.setTheme("dark");
+            props.updateTheme(props.themes["dark"]);
+        } else {
+            props.setTheme("light");
+            props.updateTheme(props.themes["light"]);
+        }
+    };
+
+    const icon = props.theme === "light" ? <MdIcons.MdDarkMode className='icon' size={25}/> : <MdIcons.MdOutlineWbSunny className='icon' size={25}/>;
+
     return (
         <>
             <div className='navbar'>
                 <div className="navbar-wrapper">
                     <div className="menu">
                         <div className="menu-bars">
-                            <FaIcons.FaBars onClick={showNavmenu}/>
+                            <FaIcons.FaBars onClick={ showNavmenu }/>
                         </div>
                         <div className="logo">
-                            <img src={Logo} alt="" />
+                            <img src={ Logo } alt="" />
                         </div>
                         <div className="dark-mode-icon">
-                            <MdIcons.MdDarkMode className='icon' size={25}/>
-                            {/* <MdIcons.MdOutlineWbSunny className='icon' size={25}/> */}
+                            <div onClick={ changeTheme } className="dark-mode-button">
+                                { icon }
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <nav className={navmenu ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-list' onClick={showNavmenu}>
+                <ul className='nav-menu-list' onClick={ showNavmenu }>
                     <li className="nav-menu-toggle">
                         <div className='menu-bars'>
                             <AiIcons.AiOutlineClose/>
@@ -39,10 +51,10 @@ const Navbar = () => {
                     </li>
                     { NavbarMenuData.map((item, index) => {
                         return (
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
+                            <li key={ index } className={ item.cName }>
+                                <Link to={ item.path }>
+                                    { item.icon }
+                                    <span>{ item.title }</span>
                                 </Link>
                             </li>
                         )
@@ -51,7 +63,7 @@ const Navbar = () => {
             </nav>
         </>
         
-    )
+    );
 }
 
 export default Navbar;
