1310d8a1647c7f76ec9bc565e3b4917f2791bd3a
krishnan2098 <krishnan2098@gmail.com>
2021-12-22 05:26:38 +0530

    Removed hamburger menu

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as MdIcons from 'react-icons/md';
-import { NavbarMenuData } from './navbar-menu-data';
-import './navbar.css';
-import Logo from '../../logo-vector.png';
+import React, { useState } from "react";
+import * as MdIcons from "react-icons/md";
+import "./navbar.css";
+import Logo from "../../logo-vector.png";
 
 const Navbar = (props) => {
-    const [navmenu, setNavmenu] = useState(false);
-    const showNavmenu = () => setNavmenu(!navmenu);
+  //   const [navmenu, setNavmenu] = useState(false);
+  //   const showNavmenu = () => setNavmenu(!navmenu);
 
-    const changeTheme = () => {
-        if (props.theme === 'light') {
-            props.setTheme("dark");
-            props.updateTheme(props.themes["dark"]);
-        } else {
-            props.setTheme("light");
-            props.updateTheme(props.themes["light"]);
-        }
-    };
+  const changeTheme = () => {
+    if (props.theme === "light") {
+      props.setTheme("dark");
+      props.updateTheme(props.themes["dark"]);
+    } else {
+      props.setTheme("light");
+      props.updateTheme(props.themes["light"]);
+    }
+  };
 
-    const icon = props.theme === "light" ? <MdIcons.MdDarkMode className='icon' size={25}/> : <MdIcons.MdOutlineWbSunny className='icon' size={25}/>;
+  const icon =
+    props.theme === "light" ? (
+      <MdIcons.MdDarkMode className="icon" size={25} />
+    ) : (
+      <MdIcons.MdOutlineWbSunny className="icon" size={25} />
+    );
 
-    return (
-        <>
-            <div className='navbar'>
-                <div className="navbar-wrapper">
-                    <div className="menu">
-                        <div className="menu-bars">
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-wrapper">
+          <div className="menu">
+            {/* <div className="menu-bars">
                             <FaIcons.FaBars onClick={ showNavmenu }/>
-                        </div>
-                        <div className="logo">
-                            <img src={ Logo } alt="" />
-                        </div>
-                        <div className="dark-mode-icon">
-                            <div onClick={ changeTheme } className="dark-mode-button">
-                                { icon }
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        </div> */}
+            <div className="logo">
+              <img src={Logo} alt="" />
             </div>
-            <nav className={navmenu ? 'nav-menu active' : 'nav-menu'}>
+            <div className="dark-mode-icon">
+              <div onClick={changeTheme} className="dark-mode-button">
+                {icon}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <nav className={navmenu ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-list' onClick={ showNavmenu }>
                     <li className="nav-menu-toggle">
                         <div className='menu-bars'>
@@ -60,10 +61,9 @@ const Navbar = (props) => {
                         )
                     })}
                 </ul>
-            </nav>
-        </>
-        
-    );
-}
+            </nav> */}
+    </>
+  );
+};
 
 export default Navbar;
