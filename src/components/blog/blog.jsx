0421d28a48dee5e8bb8fa71e485cda5f68bb6b4c
krishnan2098 <krishnan2098@gmail.com>
2022-01-13 00:10:50 +0530

    Code refactored, updated logo

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,81 +1,78 @@
-import AnimateComponent from "../../AnimateComponent";
-import "./blog.css";
+import './blog.css';
 
-const Blog = () => {
+const Blog = (props) => {
   return (
-    <AnimateComponent>
-      <div id="blog" className="blog">
-        <div className="b-section">
-          <div className="b-wrapper">
-            <div className="b-left">
-              <div className="b-row-lg">
-                <div className="post-img-lg">
-                  <img
-                    className="thumbnail"
-                    src="https://www.bing.com/th?id=OHR.USSBowfin_EN-US9736688640_1920x1080.jpg&rf=LaDigue_1920x1080.jpg"
-                    alt="post-img"
-                  />
-                </div>
-                <div className="post-meta">
-                  <span className="meta-col post-date">11 Dec</span>
-                  <span className="meta-col post-author">Al Pacino</span>
-                </div>
-                <div className="post-header">
-                  <h1>Popular Post Heading </h1>
-                </div>
-                <div className="post-subheading">
-                  <p>Enter the world of subheading of Popular Post</p>
-                </div>
+    <div id='blog' className='blog'>
+      <div className='b-section'>
+        <div className='b-wrapper'>
+          <div className='b-left'>
+            <div className='b-row-lg'>
+              <div className='post-img-lg'>
+                <img
+                  className='thumbnail'
+                  src='https://www.bing.com/th?id=OHR.USSBowfin_EN-US9736688640_1920x1080.jpg&rf=LaDigue_1920x1080.jpg'
+                  alt='post-img'
+                />
+              </div>
+              <div className='post-meta'>
+                <span className='meta-col post-date'>11 Dec</span>
+                <span className='meta-col post-author'>Al Pacino</span>
+              </div>
+              <div className='post-header'>
+                <h1>Popular Post Heading </h1>
+              </div>
+              <div className='post-subheading'>
+                <p>Enter the world of subheading of Popular Post</p>
               </div>
             </div>
-            <div className="b-right">
-              <div className="b-row">
-                <div className="post-img">
-                  <img
-                    className="thumbnail"
-                    src="https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg"
-                    alt="post-img"
-                  />
-                </div>
-                <div className="post-meta">
-                  <span className="meta-col popular post-date">11 Dec</span>
-                  <span className="meta-col popular post-author">
-                    Al Pacino
-                  </span>
-                </div>
-                <div className="post-header">
-                  <h4>Popular Post Heading </h4>
-                </div>
-                <div className="popular post-subheading">
-                  <p>Enter the world of subheading of Popular Post</p>
-                </div>
+          </div>
+          <div className='b-right'>
+            <div className='b-row'>
+              <div className='post-img'>
+                <img
+                  className='thumbnail'
+                  src='https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg'
+                  alt='post-img'
+                />
               </div>
-              <div className="b-row">
-                <div className="post-img">
-                  <img
-                    className="thumbnail"
-                    src="https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg"
-                    alt="post-img"
-                  />
-                </div>
-                <div className="post-meta">
-                  <span className="meta-col popular post-date">11 Dec</span>
-                  <span className="meta-col popular post-author">
-                    Al Pacino
-                  </span>
-                </div>
-                <div className="post-header">
-                  <h4>Popular Post Heading </h4>
-                </div>
-                <div className="popular post-subheading">
-                  <p>Enter the world of subheading of Popular Post</p>
-                </div>
+              <div className='post-meta'>
+                <span className='meta-col popular post-date'>11 Dec</span>
+                <span className='meta-col popular post-author'>
+                  Al Pacino
+                </span>
+              </div>
+              <div className='post-header'>
+                <h4>Popular Post Heading </h4>
+              </div>
+              <div className='popular post-subheading'>
+                <p>Enter the world of subheading of Popular Post</p>
+              </div>
+            </div>
+            <div className='b-row'>
+              <div className='post-img'>
+                <img
+                  className='thumbnail'
+                  src='https://www.bing.com/th?id=OHR.GlowWormBMNP_EN-US9968994144_1920x1080.jpg'
+                  alt='post-img'
+                />
+              </div>
+              <div className='post-meta'>
+                <span className='meta-col popular post-date'>11 Dec</span>
+                <span className='meta-col popular post-author'>
+                  Al Pacino
+                </span>
+              </div>
+              <div className='post-header'>
+                <h4>Popular Post Heading </h4>
+              </div>
+              <div className='popular post-subheading'>
+                <p>Enter the world of subheading of Popular Post</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </AnimateComponent>
+    </div>
   );
 };
 
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,74 +1,71 @@
 import { useRef } from 'react';
 import * as AiIcons from 'react-icons/ai';
 import emailjs from 'emailjs-com';
-import AnimateComponent from '../../AnimateComponent';
 import './contact.css';
 
-const Contact = () => {
-    const form = useRef();
+const Contact = (props) => {
+  const form = useRef();
 
-    function sendMail(e) {
-        e.preventDefault();
-        emailjs.sendForm('service_zhvels9','template_fd6l15g',form.current,'user_6cJsukfwvCrGg1RjCM7Lr')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset();
-    }
+  function sendMail(e) {
+    e.preventDefault();
+    emailjs.sendForm('service_zhvels9', 'template_fd6l15g', form.current, 'user_6cJsukfwvCrGg1RjCM7Lr')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+  }
 
-    return (
-        <AnimateComponent>
-            <div className="contact">
-                <div className="c-left">
-                    <div className="form-wrapper">
-                        <form ref={form} action="" onSubmit={sendMail}>
-                            <div className="form">
-                                <div className='fo-txt-item'><span>Let's talk!</span></div>
-                                <div className='fo-desc'><span>To request a quote or want to meet up for coffee contact me directly or fill out the form and I will get back to you soon.</span></div>
-                                <div className='fo-input-field'>
-                                    <input className="input100" type="text" name="name" placeholder='Name' required />
-                                </div>
-                                <div className='fo-input-field'>
-                                    <input className="input100" type="text" name="email" placeholder='Email Address' required />
-                                </div>
-                                <div className='fo-textarea-field'>
-                                    <textarea rows={3} name="message" placeholder='Type your message here ...' required></textarea>
-                                </div>
-                                <div>
-                                    <button>SEND MESSAGE</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className="c-right">
-                    <div className="socials-wrapper">
-                        <div className="socials">
-                            <div className='fo-txt-item'><span>Find me on</span></div>
-                            {/* <div className='fo-desc'><span>To request a quote or want to meet up for coffee contact me directly or fill out the form and I will get back to you soon.</span></div> */}
-                            <div className='row'>
-                                <div className='fo-social-icon'><AiIcons.AiFillLinkedin size={40} /></div>
-                                <div className='fo-social-col'></div>
-                                <div className='fo-social-value'><span>/ sankararaman-k</span></div>
-                            </div>
-                            <div className='row'>
-                                <div className='fo-social-icon'><AiIcons.AiFillGithub size={40} /></div>
-                                <div className='fo-social-col'></div>
-                                <div className='fo-social-value'><span>@ krishnan2098</span></div>
-                            </div>
-                            <div className='row'>
-                                <div className='fo-social-icon'><AiIcons.AiFillTwitterCircle size={40} /></div>
-                                <div className='fo-social-col'></div>
-                                <div className='fo-social-value'><span>@ krishnan2098</span></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className='contact'>
+      <div className='c-left'>
+        <div className='form-wrapper'>
+          <form ref={form} action='' onSubmit={sendMail}>
+            <div className='form'>
+              <div className='fo-txt-item'><span>Let's talk!</span></div>
+              <div className='fo-desc'><span>To request a quote or want to meet up for coffee contact me directly or fill out the form and I will get back to you soon.</span></div>
+              <div className='fo-input-field'>
+                <input className='input100' type='text' name='name' placeholder='Name' required />
+              </div>
+              <div className='fo-input-field'>
+                <input className='input100' type='text' name='email' placeholder='Email Address' required />
+              </div>
+              <div className='fo-textarea-field'>
+                <textarea rows={3} name='message' placeholder='Type your message here ...' required></textarea>
+              </div>
+              <div>
+                <button>SEND MESSAGE</button>
+              </div>
             </div>
-        </AnimateComponent>
-    )
+          </form>
+        </div>
+      </div>
+      <div className='c-right'>
+        <div className='socials-wrapper'>
+          <div className='socials'>
+            <div className='fo-txt-item'><span>Find me on</span></div>
+            {/* <div className='fo-desc'><span>To request a quote or want to meet up for coffee contact me directly or fill out the form and I will get back to you soon.</span></div> */}
+            <div className='row'>
+              <div className='fo-social-icon'><AiIcons.AiFillLinkedin size={40} /></div>
+              <div className='fo-social-col'></div>
+              <div className='fo-social-value'><span>/ sankararaman-k</span></div>
+            </div>
+            <div className='row'>
+              <div className='fo-social-icon'><AiIcons.AiFillGithub size={40} /></div>
+              <div className='fo-social-col'></div>
+              <div className='fo-social-value'><span>@ krishnan2098</span></div>
+            </div>
+            <div className='row'>
+              <div className='fo-social-icon'><AiIcons.AiFillTwitterCircle size={40} /></div>
+              <div className='fo-social-col'></div>
+              <div className='fo-social-value'><span>@ krishnan2098</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Contact;
