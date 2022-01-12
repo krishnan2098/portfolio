472fa2d2fe6e7d742f5ec73e6127553ad003f6ad
krishnan2098 <krishnan2098@gmail.com>
2022-01-13 00:10:23 +0530

    Added page transitions using framer motion, removed react router, refactored code, removed scrollbar renders

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import { AnimatePresence } from "framer-motion";
-import Navbar from "./components/navbar/navbar";
-import Intro from "./components/intro/intro";
-import Projects from "./components/projects/projects";
-import Blog from "./components/blog/blog";
-import Footer from "./components/footer/footer";
-import Contact from "./components/contact/contact";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import "./App.css";
+import Header from './components/header/header';
+import Home from './components/home/home';
+import Projects from './components/projects/projects';
+import Blog from './components/blog/blog';
+import Footer from './components/footer/footer';
+import Contact from './components/contact/contact';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
+import { wrap } from 'popmotion';
+import './App.css';
+
+// Reference: https://codesandbox.io/s/framer-motion-image-gallery-pqvx3?from-embed=&file=/src/Example.tsx:197-520
+// Reference: https://www.framer.com/docs/examples/#exit-animations
 
 const LightTheme = {
-  pageBackground: "#f5f5f5",
-  textColor: "#202020",
-  titleColor: "#FC6D6D",
-  highlightColor: "#FC6D6D",
-  navmenuBackground: "#fff",
-  navmenuHighlightColor: "#efefef",
-  blogDividerColor: "#0c0c0c",
-  scrollBorderColor: "#fff",
-  scrollThumbColor: "#505050",
-  galleryImgNumColor: "#efefef",
-  galleryBorderColor: "#efefef",
-  iconHover: "#0c0c0c",
-  inputBackground: "#d4d4d4",
-  inputFocusColor: "#d5d5d5",
-  inputBorderColor: "#000",
+  pageBackground: '#f5f5f5',
+  textColor: '#202020',
+  titleColor: '#FC6D6D',
+  highlightColor: '#FC6D6D',
+  navmenuBackground: '#fff',
+  navmenuHighlightColor: '#efefef',
+  blogDividerColor: '#0c0c0c',
+  scrollBorderColor: '#fff',
+  scrollThumbColor: '#505050',
+  galleryImgNumColor: '#efefef',
+  galleryBorderColor: '#efefef',
+  iconHover: '#0c0c0c',
+  inputBackground: '#d4d4d4',
+  inputFocusColor: '#d5d5d5',
+  inputBorderColor: '#000',
 };
 
 const DarkTheme = {
-  pageBackground: "#000",
-  textColor: "#bfc4cc",
-  titleColor: "#FC6D6D",
-  highlightColor: "#FC6D6D",
-  navmenuBackground: "#101010",
-  navmenuHighlightColor: "#0c0c0c",
-  blogDividerColor: "#bfc4cc",
-  scrollBorderColor: "#000",
-  scrollThumbColor: "#bfc4cc",
-  galleryImgNumColor: "#303030",
-  galleryBorderColor: "#202020",
-  iconHover: "#efefef",
-  inputBackground: "#131313",
-  inputFocusColor: "#202020",
-  inputBorderColor: "#bfc4cc",
+  pageBackground: '#000',
+  textColor: '#bfc4cc',
+  titleColor: '#FC6D6D',
+  highlightColor: '#FC6D6D',
+  navmenuBackground: '#101010',
+  navmenuHighlightColor: '#0c0c0c',
+  blogDividerColor: '#bfc4cc',
+  scrollBorderColor: '#000',
+  scrollThumbColor: '#bfc4cc',
+  galleryImgNumColor: '#303030',
+  galleryBorderColor: '#202020',
+  iconHover: '#efefef',
+  inputBackground: '#131313',
+  inputFocusColor: '#202020',
+  inputBorderColor: '#bfc4cc',
 };
 
 const themes = {
@@ -52,84 +55,132 @@ const themes = {
 };
 
 const App = () => {
-  const location = useLocation();
+  let pages = ['Home', 'Projects', 'Blog', 'Contact'];
+  let [[page, direction], setPage] = useState([0, 0]);
+
+  const PageIndex = wrap(0, pages.length, page);
+
+  const paginate = (newPage) => {
+    let newDirection;
+    if(newPage > page) {
+      newDirection = 1;
+    } else {
+      newDirection = -1;
+    }
+    setPage([newPage, newDirection]);
+  };
+
+  const variants = {
+    enter: (direction) => {
+      return {
+        x: direction > 0 ? 1000 : -1000,
+        opacity: 0
+      };
+    },
+    center: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1
+    },
+    exit: (direction) => {
+      return {
+        zIndex: 0,
+        x: direction < 0 ? 1000 : -1000,
+        opacity: 0
+      };
+    }
+  };
+
   const updateTheme = (theme) => {
     document.documentElement.style.setProperty(
-      "--pageBackground",
+      '--pageBackground',
       theme.pageBackground
     );
-    document.documentElement.style.setProperty("--textColor", theme.textColor);
+    document.documentElement.style.setProperty('--textColor', theme.textColor);
     document.documentElement.style.setProperty(
-      "--titleColor",
+      '--titleColor',
       theme.titleColor
     );
     document.documentElement.style.setProperty(
-      "--highlightColor",
+      '--highlightColor',
       theme.highlightColor
     );
     document.documentElement.style.setProperty(
-      "--navmenuBackground",
+      '--navmenuBackground',
       theme.navmenuBackground
     );
     document.documentElement.style.setProperty(
-      "--navmenuHighlightColor",
+      '--navmenuHighlightColor',
       theme.navmenuHighlightColor
     );
     document.documentElement.style.setProperty(
-      "--blogDividerColor",
+      '--blogDividerColor',
       theme.blogDividerColor
     );
     document.documentElement.style.setProperty(
-      "--scrollBorderColor",
+      '--scrollBorderColor',
       theme.scrollBorderColor
     );
     document.documentElement.style.setProperty(
-      "--scrollThumbColor",
+      '--scrollThumbColor',
       theme.scrollThumbColor
     );
     document.documentElement.style.setProperty(
-      "--galleryImgNumColor",
+      '--galleryImgNumColor',
       theme.galleryImgNumColor
     );
     document.documentElement.style.setProperty(
-      "--galleryBorderColor",
+      '--galleryBorderColor',
       theme.galleryBorderColor
     );
-    document.documentElement.style.setProperty("--iconHover", theme.iconHover);
+    document.documentElement.style.setProperty('--iconHover', theme.iconHover);
     document.documentElement.style.setProperty(
-      "--inputBackground",
+      '--inputBackground',
       theme.inputBackground
     );
     document.documentElement.style.setProperty(
-      "--inputFocusColor",
+      '--inputFocusColor',
       theme.inputFocusColor
     );
     document.documentElement.style.setProperty(
-      "--inputBorderColor",
+      '--inputBorderColor',
       theme.inputBorderColor
     );
   };
 
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState('dark');
   return (
     <ThemeProvider theme={themes[theme]}>
-      <div className="app">
-        <div className="app-wrapper">
-          <Navbar
+      <div className='app'>
+        <div className='app-wrapper'>
+          <Header
             theme={theme}
             themes={themes}
             updateTheme={updateTheme}
             setTheme={setTheme}
           />
-          <AnimatePresence exitBeforeEnter>
-            <Routes location={location} key={location.pathname}>
-              <Route path="/" element={<Intro />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+
+          <AnimatePresence initial={false} custom={direction} exitBeforeEnter>
+            <motion.div
+              className='app-section'
+              key={page}
+              custom={direction}
+              variants={variants}
+              initial='enter'
+              animate='center'
+              exit='exit'
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 }
+              }}
+            >
+              {PageIndex === 0 ? <Home /> : null}
+              {PageIndex === 1 ? <Projects /> : null}
+              {PageIndex === 2 ? <Blog /> : null}
+              {PageIndex === 3 ? <Contact /> : null}
+            </motion.div>
           </AnimatePresence>
-          <Footer />
+          <Footer setPage={paginate} pages={pages} currentPageIndex={pages.indexOf(page)} />
         </div>
       </div>
     </ThemeProvider>
