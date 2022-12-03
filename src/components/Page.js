4b15a7f360c1a7d1a3e16af1f2adb1355b12c7ed
krishnan2098 <krishnan2098@gmail.com>
2022-12-03 22:06:05 +0530

    Code cleanup of App.js and migrating to layout based views.

---
+++ src/components/Page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./common/Header/Header";
+
+
+export default function Page() {
+    return (
+        <div style={{height: "100vh"}}>
+            <Header/>
+        </div>        
+    );
+}
